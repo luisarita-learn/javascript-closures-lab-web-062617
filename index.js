f435af131b0dd96856ec03556bc347e4fcf01872
luisarita <github@luisarita.com>
2017-08-08 17:22:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 const app = "I don't do much."
+
+
+function bumpCounter(){
+    let counter = 0
+
+    function addBump(){
+        counter += 1
+    }
+
+    function getBumps(){
+        return counter
+    }
+
+    return {
+        addBump,
+        getBumps
+    }
+}
+
+function createAnimal(animalType){
+
+    function create(deadlyDevice){
+        return { animalType: animalType, deadlyDevice: deadlyDevice }
+    }
+
+    return create
+}
+
+let sharkCreator = createAnimal('Shark')
+let sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+let sharkWithFrickinCannon = sharkCreator('Cannon')
